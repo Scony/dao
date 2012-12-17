6385756ca8116e86a7abd541f4ff5cfe4abec0d9
Tomasz Boczkowski <penginsbacon@gmail.com>
2012-12-17 11:53:08 +0100

    Odczyt z GKeyFile. Dorzyucono grafike podswietlenia

--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -49,21 +49,63 @@ Configuration* configuration_new()
   Configuration* configuration;
 
   configuration = CONFIGURATION( g_object_new( TYPE_CONFIGURATION, NULL));
+
+  configuration->first_player = 0;
+  configuration->players[0].color = 0;
+  configuration->players[0].type = PLAYER_HUMAN;
+  configuration->players[1].type = PLAYER_HUMAN;
   
   return configuration;
 }
 
-static void configuration_set_property(GObject* object, 
+void configuration_read_from_gkeyfile(Configuration* configuration,
+				      GKeyFile* key_file)
+{
+  gchar* first_player;
+
+  first_player = g_key_file_get_string(key_file, 
+				       "Game",
+				       "first_move",
+				       NULL);
+  if(g_strcmp0(first_player, "Player2")== 0)
+    {
+      configuration->first_player = 1;
+    }
+  else
+    {
+      configuration->first_player = 0;
+    }
+
+}
+
+int configuration_read_from_data(Configuration* configuration,
+				  gchar* data, gsize length)
+{
+  GKeyFile* key_file;
+  GKeyFileFlags flags;
+  GError* error;
+
+  flags = G_KEY_FILE_KEEP_COMMENTS;
+
+  key_file = g_key_file_new();
+  if (!g_key_file_load_from_data(key_file, data, length, flags, &error))
+    {
+      return 1;
+    }
+  return 0;
+} 
+
+static void configuration_get_property(GObject* object,
 				       guint prop_id,
-				       const GValue *value,
+				       GValue* value,
 				       GParamSpec *pspec)
 {
   G_OBJECT_WARN_INVALID_PROPERTY_ID( object, prop_id, pspec);
 }
 
-static void configuration_get_property(GObject* object,
+static void configuration_set_property(GObject* object,
 				       guint prop_id,
-				       GValue* value,
+				       const GValue* value,
 				       GParamSpec *pspec)
 {
   G_OBJECT_WARN_INVALID_PROPERTY_ID( object, prop_id, pspec);
