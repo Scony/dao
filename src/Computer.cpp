3de7821b17bfd701c8eacbb17ad7e64310e2c4c9
Tomasz Boczkowski <penginsbacon@gmail.com>
2013-01-18 22:17:53 +0100

    latency

--- src/Computer.cpp
+++ src/Computer.cpp
@@ -123,6 +123,7 @@ void HillClimber::run()
   m_proposedMove = best.at();
   cout << "best H(): " << max << endl;
 
-  sleep(1);
+  int latency = Configuration::getInstance().m_latency;
+  usleep(latency);
   dispatcher_move_proposed.emit();
 }
